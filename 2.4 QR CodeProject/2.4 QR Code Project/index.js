2fd4326b23af41d367f5f9e5aaf6ce927b9d6763
Shiva Dhakal <theskdhakal@gmail.com>
2023-09-25 18:54:32 +1000

    playing around ExpressJS

--- 2.4 QR CodeProject/2.4 QR Code Project/index.js
+++ 2.4 QR CodeProject/2.4 QR Code Project/index.js
@@ -1,35 +1,35 @@
-/* 
-1. Use the inquirer npm package to get user input.
-2. Use the qr-image npm package to turn the user entered URL into a QR code image.
-3. Create a txt file to save the user input using the native fs node module.
-*/
+// /* 
+// 1. Use the inquirer npm package to get user input.
+// 2. Use the qr-image npm package to turn the user entered URL into a QR code image.
+// 3. Create a txt file to save the user input using the native fs node module.
+// */
 
-import inquirer from "inquirer";
-import qr from "qr-image";
-import fs from "fs";
-// var qr = require('qr-image');
+// import inquirer from "inquirer";
+// import qr from "qr-image";
+// import fs from "fs";
+// // var qr = require('qr-image');
 
-inquirer
-  .prompt([
-    {
-      message: "Type in your url",
-      name: "URL",
-    },
-  ])
-  .then((answers) => {
-    const url = answers.URL;
-    var qr_svg = qr.image(url);
-    qr_svg.pipe(fs.createWriteStream("qr_img.png"));
+// inquirer
+//   .prompt([
+//     {
+//       message: "Type in your url",
+//       name: "URL",
+//     },
+//   ])
+//   .then((answers) => {
+//     const url = answers.URL;
+//     var qr_svg = qr.image(url);
+//     qr_svg.pipe(fs.createWriteStream("qr_img.png"));
 
-    fs.writeFile("URL.txt", url, (err) => {
-      if (err) throw err;
-      console.log("object");
-    });
-  })
-  .catch((error) => {
-    if (error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
-    } else {
-      // Something else went wrong
-    }
-  });
+//     fs.writeFile("URL.txt", url, (err) => {
+//       if (err) throw err;
+//       console.log("object");
+//     });
+//   })
+//   .catch((error) => {
+//     if (error.isTtyError) {
+//       // Prompt couldn't be rendered in the current environment
+//     } else {
+//       // Something else went wrong
+//     }
+//   });
