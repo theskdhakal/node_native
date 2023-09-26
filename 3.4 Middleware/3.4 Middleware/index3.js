3c313c61e0565e30cfdbbc933535d0f0b6b05cd1
Shiva Dhakal <theskdhakal@gmail.com>
2023-09-26 15:58:43 +1000

    learning to use middelware

--- 3.4 Middleware/3.4 Middleware/index3.js
+++ 3.4 Middleware/3.4 Middleware/index3.js
@@ -3,6 +3,12 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+const logger = (req, res, next) => {
+  console.log("Reqest method:", req);
+  console.log("Reqest url:", req.url);
+  next();
+};
+
 app.use(logger);
 
 app.get("/", (req, res) => {
